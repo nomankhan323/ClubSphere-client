68077bb4c1e0077585cc3740565cfa1f57ffeabe
nomankhan232 <nomankhan192069@gmail.com>
2025-12-05 20:45:01 +0600

    Footer add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="p-10 text-center bg-base-200 mt-10">
+            <p>© 2025 ClubSphere — All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
